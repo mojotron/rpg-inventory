42385b74a95f4abde952061ff4c3c2335e4e9098
mojotron <igormalj@gmail.com>
2021-08-17 16:21:39 +0200

    Update character stats on log in

--- js/character_factory.js
+++ js/character_factory.js
@@ -1,20 +1,65 @@
-const characterFactory = function (userName, userPassword, classType) {
+const characterFactory = function (userName, userPassword) {
+  let hitPoints = 3;
+  let maxHitPoints = 10;
+  let attack = 1;
+  let armor = 0;
   const getName = () => userName;
   const getPassword = () => userPassword;
+  const getHitPoints = () => `${hitPoints}/${maxHitPoints}`;
+  const getAttack = () => attack;
+  const getArmor = () => armor;
+  //Character gear
+  const gear = {
+    head: null,
+    body: null,
+    legs: null,
+    leftArm: null,
+    rightArm: null,
+  };
+  const getGear = () => gear;
   //Character inventory
   const inventory = Array.from({ length: 9 }, () => null);
   const getInventory = () => inventory;
-  //Character gear
-  //object that
-
   //Currency
-  // const currency = {
-  //   gold: 10,
-  //   silver: 0,
-  //   copper: 0,
-  // }
-
+  const _currency = {
+    gold: 4,
+    silver: 99,
+    copper: 99,
+  };
+  const getCurrency = function () {
+    const { gold, silver, copper } = _currency;
+    return [gold, silver, copper];
+  };
   //Character Actions
-
-  return { getName, getPassword, getInventory };
+  return {
+    getName,
+    getPassword,
+    getInventory,
+    getHitPoints,
+    getAttack,
+    getArmor,
+    getCurrency,
+    getGear,
+  };
 };
+
+const itemFactory = function () {};
+
+//base
+//🧦
+//
+// const dagger 🔪
+// const axe 🪓
+// const sword 🗡️
+// const bow 🏹
+// shield 🛡️
+// mace 🏏
+// legs lv 1 👟
+// legs lv 2 👞
+// legs lv 3 🥾
+// body lv 1 🎽
+// body lv 2 👕
+// body lv 3 🧥
+// head lv 1 🧣
+// head lv 2 🧢
+// head lv 3 👑
--- js/main.js
+++ js/main.js
@@ -1,22 +1,58 @@
 'use strict';
-//create characters
-const warrior = characterFactory('Stomp', 111, 'warrior');
-const wizard = characterFactory('Cast', 222, 'wizard');
-const ranger = characterFactory('Draw', 333, 'ranger');
-
-const loginCharName = document.querySelector('.login-user-character');
-const loginCharPass = document.querySelector('.login-user-password');
-const loginBtn = document.querySelector('.login-btn');
-loginBtn.addEventListener('click', function (e) {
-  e.preventDefault();
-  //TODO
-  if (loginCharName.value === 'Bob' && +loginCharPass.value === 111) {
-    document.querySelector('.game-display').style.opacity = '1';
-  }
-  //Clear input
-  loginCharName.value = '';
-  loginCharPass.value = '';
-  loginCharPass.blur();
-  loginCharName.blur();
-  //
-});
+//Game engin
+(function () {
+  let currentChar;
+  //pre made characters
+  const stomp = characterFactory('Stomp', 111);
+  const cast = characterFactory('Cast', 222);
+  const draw = characterFactory('Draw', 333);
+  const characters = [stomp, cast, draw];
+  //Selectors
+  const gameApp = document.querySelector('.game-display');
+  //login selectors
+  const loginCharName = document.querySelector('.login-user-character');
+  const loginCharPass = document.querySelector('.login-user-password');
+  const loginBtn = document.querySelector('.login-btn');
+  //Character ui selectors
+  const charName = document.querySelector('.character-name');
+  const charHP = document.querySelector('.character-hit-points');
+  const charAttack = document.querySelector('.character-attack');
+  const charArmor = document.querySelector('.character-armor');
+  const charGold = document.querySelector('.currency-gold');
+  const charSilver = document.querySelector('.currency-silver');
+  const charCopper = document.querySelector('.currency-copper');
+  //LOG IN USER
+  loginBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    //Find character
+    currentChar = characters.find(
+      char => char.getName() === loginCharName.value
+    );
+    if (currentChar?.getPassword() === +loginCharPass.value) {
+      //Update Char stats UI
+      charName.textContent = currentChar.getName();
+      charHP.textContent = currentChar.getHitPoints();
+      charAttack.textContent = currentChar.getAttack();
+      charArmor.textContent = currentChar.getArmor();
+      charGold.textContent = currentChar.getCurrency()[0];
+      charSilver.textContent = currentChar.getCurrency()[1];
+      charCopper.textContent = currentChar.getCurrency()[2];
+      //Update Char Gear
+      for (const [key, value] of Object.entries(currentChar.getGear())) {
+        const gearEl = document.querySelector(`[data-${key}=""]`);
+        gearEl.textContent = value;
+      }
+      //Update Char Inventory
+      currentChar.getInventory().forEach(function (slot, i) {
+        const slotEl = document.querySelector(`[data-slot="${i}"]`);
+        slotEl.textContent = slot;
+      });
+      //
+      gameApp.style.opacity = 1;
+    }
+    //Clear input
+    loginCharName.value = loginCharPass.value = '';
+    loginCharPass.blur();
+    loginCharName.blur();
+  });
+})();
